a6722f2d3e1aba62786dd94fb16520130b24fe7a
JenChieh <jcs090218@gmail.com>
2024-06-04 14:35:25 -0700

    feat: impls function pointer

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -13,7 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+pub mod room;
+
 use crate::channel::*;
+use crate::handler::room::*;
 use crate::room::*;
 use serde_json::Value;
 use std::sync::Arc;
@@ -24,25 +27,24 @@ pub async fn handle(channel: &mut Channel, room: &Arc<Mutex<Room>>, json: &str) 
     let val: Value = v.unwrap();
     let method: &str = val["method"].as_str().unwrap();
 
-    match method {
-        "test" => {
-            test::handle(channel, room, &val).await;
-        }
-        "ping" => {
-            ping::handle(channel, room, &val).await;
-        }
-        "enter" => {
-            enter::handle(channel, room, &val).await;
-        }
-        "exit" => {
-            exit::handle(channel, room, &val).await;
-        }
-        _ => {
-            tracing::error!("Unkown method request: {:?}", method);
-        }
+    let function = match method {
+        "test" => Some(test::handle),
+        "ping" => Some(ping::handle),
+        "enter" => Some(enter::handle),
+        "exit" => Some(exit::handle),
+        "broadcast" => Some(broadcast::handle),
+        _ => None,
+    };
+
+    if function.is_none {
+        tracing::error!("Unkown method request: {:?}", method);
+        return;
     }
+
+    function(channel, room, &val).await;
 }
 
+/// Test
 mod test {
     use crate::channel::*;
     use crate::room::*;
@@ -64,7 +66,7 @@ mod test {
             .await;
 
         channel.broadcast_json(&serde_json::json!({
-            "method": "test_broadcast",
+            "method": "test",
             "message": "This is the broadcast test!",
         }));
     }
@@ -84,103 +86,6 @@ mod ping {
             .send_json(&serde_json::json!({
                 "method": "pong",
                 "timestamp": chrono::offset::Local::now().to_string(),
-            }))
-            .await;
-    }
-}
-
-/// Enter room
-mod enter {
-    use crate::channel::*;
-    use crate::room::*;
-    use serde_json::Value;
-    use std::sync::Arc;
-    use tokio::sync::Mutex;
-
-    pub async fn handle(channel: &mut Channel, room: &Arc<Mutex<Room>>, json: &Value) {
-        let addr = &channel.get_connection().addr;
-        let mut room = room.lock().await;
-        let client = room.get_client_mut(addr).unwrap();
-
-        if client.entered() {
-            channel
-                .send_json(&serde_json::json!({
-                    "method": "enter",
-                    "message": "You have already entered the room",
-                    "status": "failure",
-                }))
-                .await;
-            return;
-        }
-
-        let username = json["username"].to_string();
-        let password = if json["password"].is_null() {
-            None
-        } else {
-            Some(json["password"].to_string())
-        };
-
-        let (entered, message) = room.enter(addr, &username, &password);
-
-        if entered {
-            // Update client info!
-            {
-                let client = room.get_client_mut(addr).unwrap();
-                client.enter_room(Some(username.clone()));
-            }
-
-            channel
-                .send_json(&serde_json::json!({
-                    "method": "enter",
-                    "message": "You have successully entered the room",
-                    "username": username,
-                    "status": "success",
-                }))
-                .await;
-        } else {
-            channel
-                .send_json(&serde_json::json!({
-                    "method": "enter",
-                    "message": message,
-                    "status": "failure",
-                }))
-                .await;
-        }
-    }
-}
-
-/// Enter room
-mod exit {
-    use crate::channel::*;
-    use crate::room::*;
-    use serde_json::Value;
-    use std::sync::Arc;
-    use tokio::sync::Mutex;
-
-    pub async fn handle(channel: &mut Channel, room: &Arc<Mutex<Room>>, _json: &Value) {
-        let addr = &channel.get_connection().addr;
-        let mut room = room.lock().await;
-        let client = room.get_client_mut(addr).unwrap();
-
-        if !client.entered() {
-            channel
-                .send_json(&serde_json::json!({
-                    "method": "exit",
-                    "message": "You never entered the room; do nothing",
-                    "status": "failure",
-                }))
-                .await;
-            return;
-        }
-
-        // Leave the room
-        client.exit_room();
-
-        channel
-            .send_json(&serde_json::json!({
-                "method": "exit",
-                "message": "You have successfully left the room",
-                "status": "success",
             }))
             .await;
     }
